fd56be1492354ee3df0913b9b096edaeb4e49e76
sashimi-oss <piagita8@gmail.com>
2024-10-20 14:05:07 +0900

    音声素材修正及びそれに伴う表示の修正

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const display = document.querySelector('.display');
 // display.insertAdjacentHTML('beforeend', `<h3>音声1-1</h3><audio controls src="./test.wav"></audio>`);
 
-function displayWav(wavNum){
+function displayWav(labelNum, wavNum){
   if(wavNum == 0) {
-    display.insertAdjacentHTML('beforeend', `<h3>音声${wavNum}</h3><audio controls src="./test.wav"></audio>`);
+    display.insertAdjacentHTML('beforeend', `<h3 id="${labelNum}">音声${labelNum}</h3><audio controls src="./test.wav"></audio>`);
   } else {
-    display.insertAdjacentHTML('beforeend', `<h3>音声${wavNum}</h3><audio controls src="./test (${wavNum}).wav"></audio>`);
+    display.insertAdjacentHTML('beforeend', `<h3 id="${labelNum}">音声${labelNum}</h3><audio controls src="./test (${wavNum}).wav"></audio>`);
   }
 }
-for(let i = 0; i < 5; i++){
-  displayWav(i);
+let labelNum = 0;
+for(let i = 0; i < 60; i++){
+  if(i == 20 || i == 43) {
+    continue;
+  }
+  labelNum++;
+  displayWav(labelNum,i);
 }
